eca9883cadab136d7ad17dd609244d10576a2390
Nanda7310 <168728205+Nanda7310@users.noreply.github.com>
2024-06-22 18:50:54 +0530

    Add files via upload

---
+++ quiz_backend/src/main/java/com/nandu/quizonline/QuizOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.nandu.quizonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizOnlineApplication.class, args);
+    }
+
+}
